aad95302103dc5387454136d5c11992110945178
jiangwei <1753120307@qq.com>
2018-08-30 20:11:29 +0800

    tt

--- src/main/java/com/pe/exchange/utils/UserUtil.java
+++ src/main/java/com/pe/exchange/utils/UserUtil.java
@@ -3,6 +3,7 @@ package com.pe.exchange.utils;
 public class UserUtil {
     private static ThreadLocal<Integer> USER_ID=new ThreadLocal<>();
     public static Integer get(){
+       System.out.print(1);
        return USER_ID.get();
     }
     public static void set(Integer userId){
