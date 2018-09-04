ed9cfeb4d24630a6e86c9e23a15993a492eef01a
jiangwei <1753120307@qq.com>
2018-09-04 23:00:54 +0800

    修改用户过期时间，添加处理方式

--- src/main/java/com/pe/exchange/entity/DKOrder.java
+++ src/main/java/com/pe/exchange/entity/DKOrder.java
@@ -2,8 +2,19 @@ package com.pe.exchange.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
 public class DKOrder implements Serializable{
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private Integer userId;
 	private String orderNumber;
