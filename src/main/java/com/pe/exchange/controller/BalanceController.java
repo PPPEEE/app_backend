b463009da694d1363629190c65cfcb56f1475a05
xiecan <xie.can@outlook.com>
2018-09-08 22:52:12 +0800

    完善swagger描述,返回消息中去掉没必要的字段

--- src/main/java/com/pe/exchange/controller/BalanceController.java
+++ src/main/java/com/pe/exchange/controller/BalanceController.java
@@ -2,8 +2,11 @@ package com.pe.exchange.controller;
 
 import com.pe.exchange.common.Result;
 import com.pe.exchange.common.Results;
+import com.pe.exchange.entity.UserBalance;
+import com.pe.exchange.entity.UserBonusLog;
 import com.pe.exchange.service.TransferService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
 import lombok.Data;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Slf4j
 @Api(tags = "收支记录")
 @RestController
@@ -22,6 +27,7 @@ public class BalanceController {
 
     @Autowired TransferService transferService;
     @Data
+    @ApiModel
     public static class IncomeListBean {
         @ApiModelProperty(required = true,value = "币种,0DK,1DN")
         private int coinType;
@@ -30,6 +36,7 @@ public class BalanceController {
     }
 
     @Data
+    @ApiModel
     public static class BalanceBean {
         @ApiModelProperty(required = true,value = "币种,0DK,1DN")
         private int coinType;
@@ -38,19 +45,19 @@ public class BalanceController {
 
     @ApiOperation("获取全部余额")
     @GetMapping("get")
-    public Result getAllBalance(){
+    public Result<List<UserBalance>> getAllBalance(){
         return Results.success(transferService.getBalance());
     }
 
     @ApiOperation("获取余额")
     @GetMapping("getby")
-    public Result getBalance(@RequestBody BalanceBean balanceBean){
+    public Result<UserBalance> getBalance(@RequestBody BalanceBean balanceBean){
         return Results.success(transferService.getBalance(balanceBean.getCoinType()));
     }
 
     @ApiOperation("收支记录")
     @GetMapping("incomeList")
-    public Result incomeList(@RequestBody IncomeListBean incomeListBean){
+    public Result<List<UserBonusLog>> incomeList(@RequestBody IncomeListBean incomeListBean){
         return Results.success(transferService.getIncomeList(incomeListBean.coinType,incomeListBean.incomeType));
     }
 }
--- src/main/java/com/pe/exchange/entity/UserBalance.java
+++ src/main/java/com/pe/exchange/entity/UserBalance.java
@@ -1,5 +1,7 @@
 package com.pe.exchange.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,32 +17,39 @@ import java.math.BigDecimal;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel
 public class UserBalance {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(hidden = true)
     private Integer id;
 
+    @ApiModelProperty(hidden = true)
     private Integer userId;
     /**
      * 币种类型,0、平台DK 1、平台DN
      */
 
+    @ApiModelProperty(value = "币种类型,0、平台DK 1、平台DN",position = 0)
     private Integer coinType;
 
     /**
      * 收款地址
      */
+    @ApiModelProperty(value = "在址,暂时为空",position = 1)
     private String address;
     /**
      *  余额
      */
     @Column(precision = 19,scale = 6)
+    @ApiModelProperty(value = "余额",position = 2)
     private BigDecimal balance;
 
     /**
      * 锁定余额
      */
     @Column( precision = 19,scale = 6)
+    @ApiModelProperty(hidden = true)
     private BigDecimal lockBalance;
 
 
